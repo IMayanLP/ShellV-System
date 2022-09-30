af78a2542ae0ef61394f61086f752686c8f1f299
imayanlp <mayan20119@gmail.com>
2022-09-30 18:37:33 -0300

    Pequena alteração na funcionalidade do menu

--- menu.sh
+++ menu.sh
@@ -3,14 +3,16 @@ MENU=1
 clear
 while :
 do
-	echo "Digite o número da questão que você quer executar: "
+	echo "Digite o número da questão que você quer executar (1 a 13): "
+	echo "Ou digite 0 (zero) para SAIR "
 	read MENU
 	if [ $MENU -eq 0 ] ; then
 		clear
 		break
 	fi
-	if [ $MENU -ge 1 ] && [ $MENU -le 12 ] ; then
+	if [ $MENU -ge 1 ] && [ $MENU -le 13 ] ; then
 		bash "Q$MENU.sh"
+		echo " "
 		echo "Fim! Pressione ENTER para continuar... :D"
 		read && clear
 		else
