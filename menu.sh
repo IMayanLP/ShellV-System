ff4ee5401b948212032ad1b9fc2b55543d9bdfe7
imayanlp <mayan20119@gmail.com>
2022-09-30 19:35:29 -0300

    Atualizando o visual do menu a fim de melhorar a experiência do usuário

--- menu.sh
+++ menu.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 MENU=1
 clear
+
+CORES=('\033[0;31m' '\033[0;32m' '\033[0;33m' '\033[0;34m' '\033[0;35m' '\033[0;36m')
+COR_PADRAO='\033[0m'
+COR_ATUAL=0
+
 while :
 do
+	echo -e "${CORES[$COR_ATUAL]}"
+	echo "
+	███╗   ███╗███████╗███╗   ██╗██╗   ██╗
+	████╗ ████║██╔════╝████╗  ██║██║   ██║
+	██╔████╔██║█████╗  ██╔██╗ ██║██║   ██║
+	██║╚██╔╝██║██╔══╝  ██║╚██╗██║██║   ██║
+	██║ ╚═╝ ██║███████╗██║ ╚████║╚██████╔╝
+	╚═╝     ╚═╝╚══════╝╚═╝  ╚═══╝ ╚═════╝ "
 	echo "Digite o número da questão que você quer executar (1 a 13): "
 	echo "Ou digite 0 (zero) para SAIR "
 	read MENU
@@ -11,7 +24,9 @@ do
 		break
 	fi
 	if [ $MENU -ge 1 ] && [ $MENU -le 13 ] ; then
+		echo -e "$COR_PADRAO"
 		bash "Q$MENU.sh"
+		echo -e "${CORES[$COR_ATUAL]}"
 		echo " "
 		echo "Fim! Pressione ENTER para continuar... :D"
 		read && clear
@@ -19,5 +34,10 @@ do
 			echo "Questão inválida! pressione ENTER para tente novamente... :("
 			read && clear
 			continue
+	fi
+	if [ $COR_ATUAL -eq 5 ] ; then
+		COR_ATUAL=0
+	else
+		COR_ATUAL=$((COR_ATUAL+1))
 	fi
 done
